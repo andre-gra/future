ea119d50dbf7644f579e1ae6fa9400091176793a
andregra <andregra717@gmail.com>
2022-01-12 12:01:59 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,34 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: 'https://www.yourdomain.tld',
   },
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
+        version: 'draft',
+        /* languages: ['de', 'at'] // Optional parameter.
+        Omission will retrieve all languages by default. */
+      },
     },
   ],
 };
